3d45d760290b1297107449ff73b1dc4121a7d1c0
JaMarko <jakubamark0@gmail.com>
2021-07-20 12:38:31 +0200

    Dodaj pliki projektów.

---
+++ Scalac_staz_zadanie/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Scalac_staz_zadanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        List<double> frame_pixels = new List<double>();
+        List<double> mid_pixels = new List<double>();
+        List<double> weights = new List<double>();
+        string file_name;
+        Image loaded_image;
+        int dot_position;
+        string str_brightness;
+        int cut_off = 70;
+        double frame_ratio = 0.2;
+        double frame_weight = 0.5;
+        double mid_weight = 1.5;
+        int brightness;
+        double num = 0; //numerator in brightness equasion
+        double den = 0; //denominator in brightness equasion
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(776, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Process File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(12, 29);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(776, 22);
+            this.inputBox.TabIndex = 1;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 108);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(776, 22);
+            this.outputBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "input file directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "output file directory";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "processing...";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Picture analysis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Scalac_staz_zadanie/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Scalac_staz_zadanie
+{
+    public partial class Form1 : Form
+    {
+        Bitmap loaded_bitmap;
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            inputBox.Enabled = false;
+            outputBox.Enabled = false;
+            label3.Visible = false;
+            string[] loaded_Files = Directory.GetFiles(@inputBox.Text);
+            process_images(loaded_Files);
+        }
+
+        private void process_images(string[] files)
+        {
+            foreach (string file in files)
+            {
+                pixel_defining(file);   //defines brightness of every pixel
+                brightness = count_brightness(frame_pixels, mid_pixels, weights);   //counts overall brightness of a picture
+                str_brightness = bright_check(brightness);  //checks whether the picture is "dark" or "bright"
+                save_images(file, brightness, str_brightness);  //saves the image to new directory with changed name
+            }
+            label3.Text = "processing is done!";
+            label3.Visible = true;
+            button1.Enabled = true;
+            inputBox.Enabled = true;
+            outputBox.Enabled = true;
+        }
+
+        private void save_images(string file, int brightness, string str_brightness)
+        {
+            file_name = Path.GetFileName(file);
+            dot_position = file_name.IndexOf(".");
+            file_name = file_name.Insert(dot_position, "_" + str_brightness + "_" + brightness);    //composes final name of the file
+            string directory = outputBox.Text + "\\" + file_name;
+            loaded_image.Save(directory);
+        }
+
+        private string bright_check(int br)
+        {
+            if (br > cut_off)
+                return "dark";
+            else
+                return "bright";
+        }
+
+        private void pixel_defining(string file)
+        {
+            loaded_image = Image.FromFile(file);
+            loaded_bitmap = new Bitmap(loaded_image);
+            for (int x = 0; x < loaded_bitmap.Width; x++)
+            {
+                for (int y = 0; y < loaded_bitmap.Height; y++)
+                {
+                    Color pixel_color = loaded_bitmap.GetPixel(x, y);
+                    if (is_frame(loaded_bitmap, x, y))  //checks whether the pixel is on the edge or in the middle of picture (different importance)
+                    {
+                        frame_pixels.Add(pixel_color.GetBrightness());
+                        weights.Add(frame_weight);
+                    }
+                    else
+                    {
+                        mid_pixels.Add(pixel_color.GetBrightness());
+                        weights.Add(mid_weight);
+                    }
+                }
+            }
+        }
+
+        private bool is_frame(Bitmap pic, int width, int height)
+        {
+            if (width > pic.Width * frame_ratio && width < pic.Width - (pic.Width * frame_ratio))
+            {
+                if (height > pic.Height * frame_ratio && height < pic.Height - (pic.Height * frame_ratio))
+                    return false;
+                else
+                    return true;
+            }
+            else return true;
+        }
+
+        private int count_brightness(List<double> frame, List<double> mid, List<double> weights)
+        {
+            foreach(double x in frame)
+            {
+                num += x * frame_weight;
+            }
+            frame.Clear();
+            foreach (double x in mid)
+            {
+                num += x * mid_weight;
+            }
+            mid.Clear();
+            foreach (double x in weights)
+            {
+                den += x;
+            }
+            weights.Clear();
+            if (den != 0)
+                brightness = 100 - (int)(100 * (num / den));
+            else
+                brightness = 0;
+            num = 0;
+            den = 0;
+            return brightness;
+        }
+    }
+}
